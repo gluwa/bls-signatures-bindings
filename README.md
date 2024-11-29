dccc3ad72b3e108f273efde3449e7928567b75a0
Beqa Abuladze <beqaabuladze.00@gmail.com>
2024-11-29 20:35:11 +0400

    chore: readme and example

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # TS Bindings for `no_std` BLS Signatures
 
 Creates bindings for https://github.com/gluwa/bls-signatures. `./pkg` needs to be published to npm
+
+## Usage
+
+Inside `ts-example` there is an example of how to use the bindings. You should additionally make a symbolic link inside the dir.
+```bash
+ln -s ../pkg ./pkg
+```
+
+After that you can build the project with
+```bash
+npm run build
+```
+
+and then run it
+```bash
+npm start
+```
---
+++ ts-example/src/test.ts
@@ -0,0 +1,20 @@
+import { WasmPrivateKey } from '../pkg/bls_signatures_bindings.js';
+
+async function main() {
+    const seed = new Uint8Array(32);
+    console.log("seed", seed);
+
+    const privateKey = WasmPrivateKey.generate(seed);
+    console.log("privateKey", privateKey.as_bytes());
+
+    const publicKey = privateKey.public_key();
+    console.log("publicKey", publicKey.as_bytes());
+
+    const message = new TextEncoder().encode("Hello, World!");
+    const signature = privateKey.sign(message);
+
+    const isValid = publicKey.verify(signature, message);
+    console.log("Signature valid:", isValid);
+}
+
+main().then(r => console.log("Done!")).catch(e => console.error(e));
